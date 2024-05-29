a1574eb454ea99f0b8940853ebfe49063873dbb6
chandrakumar <>
2024-05-29 12:23:50 +0530

    Cloud watch agent configuration several type of architecture

--- scripts/cloudwatch-installation-script.sh
+++ scripts/cloudwatch-installation-script.sh
@@ -6,3 +6,6 @@ rpm -U ./amazon-cloudwatch-agent.rpm
 cp <cloudwatch_agent_path> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 #start the cludwatch agent in background
 /opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent & 
+
+# Cloudwatch agent configuration for several type of Architecture
+# https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/download-cloudwatch-agent-commandline.html
